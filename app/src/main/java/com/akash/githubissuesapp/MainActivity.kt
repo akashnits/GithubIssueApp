958b1eb46c36429bcd6479777b627bcd1ead4546
Akash Raj <akash.raj@cybernetyx.com>
2020-07-26 06:46:06 +0530

    navigation fixes

--- app/src/main/java/com/akash/githubissuesapp/MainActivity.kt
+++ app/src/main/java/com/akash/githubissuesapp/MainActivity.kt
@@ -4,14 +4,22 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import dagger.android.AndroidInjector
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() , HasSupportFragmentInjector{
+
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
@@ -19,6 +27,25 @@ class MainActivity : AppCompatActivity() , HasSupportFragmentInjector{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController= findNavController(R.id.container)
+        // Set up the ActionBar to stay in sync with the NavController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment),
+            null)
+
+        setupActionBar(navController, appBarConfiguration)
+
+    }
+
+    private fun setupActionBar(navController: NavController,
+                               appBarConfig : AppBarConfiguration) {
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 
     override fun supportFragmentInjector()= dispatchingAndroidInjector
@@ -28,5 +55,11 @@ class MainActivity : AppCompatActivity() , HasSupportFragmentInjector{
             val inputMethodManager = ContextCompat.getSystemService(this, InputMethodManager::class.java)!!
             inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // Allows NavigationUI to support proper up navigation or the drawer layout
+        // drawer menu, depending on the situation
+        return findNavController(R.id.container).navigateUp()
     }
 }
